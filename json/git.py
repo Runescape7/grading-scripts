97a95c7df3e908c0f33571316497e19fc93e71f8
Stoney Jackson <dr.stoney@gmail.com>
2013-06-03 23:14:13 -0400

    Undested additions to git.py

--- json/git.py
+++ json/git.py
@@ -4,39 +4,52 @@ Instead, it only provides access to Git needed by the grading-script
 project.
 '''
 import logging
+import subprocess
 
-def checkoutRemote(remoteName, remoteUrl, remoteBranch, localBranch) :
-  '''
-  git remote add -f remoteName url
-  git checkout -b localBranch remoteName/remoteBranch
-  '''
-  logging.debug("checkoutRemote("+remoteName+","+remoteUrl+","+remoteBranch+","+localBranch+")")
-  pass
+def removeRemote(remoteName) :
+  ''' git remote remove remoteName '''
+  logging.debug("git.removeRemote("+remoteName+")")
+  return subprocess.call(['git', 'remote', 'remove', remoteName])
+
+def addRemote(remoteName, remoteUrl) :
+  ''' git remote add -f remoteName url '''
+  logging.debug(
+    "git.addRemote("+
+    remoteName+","+
+    remoteUrl+","+
+    remoteBranch+","+
+    localBranch+")"
+  )
+  r = subprocess.call(['git', 'remote', 'add', '-f', remoteName, remoteUrl]) 
+  if r == 1 : 
+    removeRemote(remoteName)
+    return 1
+  return 0
+
+def checkoutRemote(newBranch, remoteName, remoteBranch) :
+  logging.debug(
+    "git.checkoutRemote("+newBranch+","+remoteName+","+remoteBranch+")")
+  return subprocess.call(
+    ['git','checkout','-b',newBranch,remoteName+'/'+remoteBranch]
+  )
 
 def checkout(localBranch) :
-  '''
-  git checkout localBranch
-  '''
-  logging.debug("checkout("+localBranch+")")
-  pass
+  ''' git checkout localBranch '''
+  logging.debug("git.checkout("+localBranch+")")
+  return subprocess.call(['git','checkout',localBranch])
 
 def add(file) :
-  '''
-  git add file
-  '''
-  pass
+  ''' git add file '''
+  logging.debug("git.add("+file+")")
+  return subprocess.call(['git','add',file])
 
 def commit(message) :
-  '''
-  git commit -m'message'
-  '''
+  ''' git commit -m'message' '''
   logging.debug("git.commit("+message+")")
-  pass
+  return subprocess.call(['git','commit','-m',message])
 
 def push(remote, branch) :
-  '''
-  git push remote branch
-  '''
+  ''' git push remote branch '''
   logging.debug("git.push("+remote+','+branch+")")
-  pass
+  return subprocess.call(['git','push',remote,branch])
 
