fd3cfee5744f3d52d05c6757be6c70ed79eb57b2
Stoney Jackson <dr.stoney@gmail.com>
2013-06-04 23:03:25 -0400

    Fix authorship.

--- lab1FetchConvert.sh
+++ lab1FetchConvert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2013 Karl R. Wurst
+# Copyright (C) 2013 Karl R. Wurst, Stoney Jackson
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
